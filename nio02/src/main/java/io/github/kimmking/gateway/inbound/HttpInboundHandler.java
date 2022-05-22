cf28e5f5d4c45a3638870ab726f648c88a232044
chenxinpeixiting@163.com <chenxinpeixiting@163.com>
2022-05-22 22:04:11 +0800

    第三周作业，第三题

--- nio02/src/main/java/io/github/kimmking/gateway/inbound/HttpInboundHandler.java
+++ nio02/src/main/java/io/github/kimmking/gateway/inbound/HttpInboundHandler.java
@@ -2,6 +2,7 @@ package io.github.kimmking.gateway.inbound;
 
 import io.github.kimmking.gateway.filter.HeaderHttpRequestFilter;
 import io.github.kimmking.gateway.filter.HttpRequestFilter;
+import io.github.kimmking.gateway.filter.MyRequestFilter;
 import io.github.kimmking.gateway.outbound.httpclient4.HttpOutboundHandler;
 import io.github.kimmking.gateway.outbound.okhttp.OkhttpOutboundHandler;
 import io.netty.channel.ChannelHandlerContext;
@@ -18,7 +19,7 @@ public class HttpInboundHandler extends ChannelInboundHandlerAdapter {
     private static Logger logger = LoggerFactory.getLogger(HttpInboundHandler.class);
     private final List<String> proxyServer;
     private OkhttpOutboundHandler handler;
-    private HttpRequestFilter filter = new HeaderHttpRequestFilter();
+    private HttpRequestFilter filter = new MyRequestFilter();
     
     public HttpInboundHandler(List<String> proxyServer) {
         this.proxyServer = proxyServer;
